74f50bbee0581968d7151fd3febef9aebe8189d3
JoDan Alberts <jodan@labs.epiuse.com>
2018-08-01 21:51:01 +0200

    Add EduAnywhere images to homepage
    Removes unused buttons from navbar and adds images to landing page.

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -9,7 +9,9 @@ import {
   VBtn,
   VIcon,
   VGrid,
-  VToolbar
+  VToolbar,
+  VParallax,
+  VCarousel
 } from 'vuetify'
 
 Vue.use(Vuetify, {
@@ -22,6 +24,8 @@ Vue.use(Vuetify, {
     VBtn,
     VIcon,
     VGrid,
-    VToolbar
+    VToolbar,
+    VParallax,
+    VCarousel
   }
 })
