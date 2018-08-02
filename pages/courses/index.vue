492c0d527f266c426e39bc3de1643b957f197b53
JoDan Alberts <jodan@labs.epiuse.com>
2018-08-02 22:59:46 +0200

    Add layout for courses page

--- pages/courses/index.vue
+++ pages/courses/index.vue
@@ -1,181 +1,33 @@
 <template>
     <v-app light>
-        <v-content>
-            <section>
-                <v-parallax src='/images/blackboard-trees.jpg' height="400">
-                    <v-layout
-                            column
-                            align-center
-                            justify-center
-                            class="white--text"
-                    >
-                        <img src="/images/EduAnywhere.png" alt="Vuetify.js" height="200">
-                        <h1 class="white--text mb-2 display-1 text-xs-center">Parallax Template</h1>
-                        <div class="subheading mb-3 text-xs-center">Powered by Vuetify</div>
-                        <v-btn
-                                class="blue lighten-2 mt-5"
-                                dark
-                                large
-                                href="/pre-made-themes"
-                        >
-                            Get Started
-                        </v-btn>
-                    </v-layout>
-                </v-parallax>
-            </section>
-
-            <section>
-                <v-layout
-                        column
-                        wrap
-                        class="my-5"
-                        align-center
-                >
-                    <v-flex xs12 sm4 class="my-3">
-                        <div class="text-xs-center">
-                            <h2 class="headline">The best way to start developing</h2>
-                            <span class="subheading">
-                Cras facilisis mi vitae nunc
-              </span>
-                        </div>
+        <v-container fluid grid-list-md text-xs-center>
+            <v-content>
+                <v-layout row wrap>
+                    <v-flex xs4>
+                        <v-card class="theme--light">
+                            <v-card-text class="px-0">Hello World</v-card-text>
+                        </v-card>
                     </v-flex>
-                    <v-flex xs12>
-                        <v-container grid-list-xl>
-                            <v-layout row wrap align-center>
-                                <v-flex xs12 md4>
-                                    <v-card class="elevation-0 transparent">
-                                        <v-card-text class="text-xs-center">
-                                            <v-icon x-large class="blue--text">color_lens</v-icon>
-                                        </v-card-text>
-                                        <v-card-title primary-title class="black--text layout justify-center">
-                                            <div class="headline text-xs-center">Material Design</div>
-                                        </v-card-title>
-                                        <v-card-text class="black--text">
-                                            Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt
-                                            ornare.
-                                            Pellentesque habitant morbi tristique senectus et netus et malesuada fames
-                                            ac turpis egestas.
-                                            Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt.
-                                            Suspendisse potenti.
-                                        </v-card-text>
-                                    </v-card>
-                                </v-flex>
-                                <v-flex xs12 md4>
-                                    <v-card class="elevation-0 transparent">
-                                        <v-card-text class="text-xs-center">
-                                            <v-icon x-large class="blue--text">flash_on</v-icon>
-                                        </v-card-text>
-                                        <v-card-title primary-title class="black--text layout justify-center">
-                                            <div class="headline">Fast development</div>
-                                        </v-card-title>
-                                        <v-card-text class="black--text">
-                                            Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt
-                                            ornare.
-                                            Pellentesque habitant morbi tristique senectus et netus et malesuada fames
-                                            ac turpis egestas.
-                                            Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt.
-                                            Suspendisse potenti.
-                                        </v-card-text>
-                                    </v-card>
-                                </v-flex>
-                                <v-flex xs12 md4>
-                                    <v-card class="elevation-0 transparent">
-                                        <v-card-text class="text-xs-center">
-                                            <v-icon x-large class="blue--text">build</v-icon>
-                                        </v-card-text>
-                                        <v-card-title primary-title class="black--text layout justify-center">
-                                            <div class="headline text-xs-center">Completely Open Sourced</div>
-                                        </v-card-title>
-                                        <v-card-text class="black--text">
-                                            Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt
-                                            ornare.
-                                            Pellentesque habitant morbi tristique senectus et netus et malesuada fames
-                                            ac turpis egestas.
-                                            Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt.
-                                            Suspendisse potenti.
-                                        </v-card-text>
-                                    </v-card>
-                                </v-flex>
-                            </v-layout>
-                        </v-container>
+
+                    <v-flex xs4>
+                        <v-card class="theme--light">
+                            <v-card-text class="px-0">Hello World</v-card-text>
+                        </v-card>
+                    </v-flex>
+
+                    <v-flex xs4>
+                        <v-card class="theme--light">
+                            <v-card-text class="px-0">Hello World</v-card-text>
+                        </v-card>
+                    </v-flex>
+
+                    <v-flex xs4>
+                        <v-card class="theme--light">
+                            <v-card-text class="px-0">Hello World</v-card-text>
+                        </v-card>
                     </v-flex>
                 </v-layout>
-            </section>
-
-            <section>
-                <v-parallax src="/images/section.jpg" height="380">
-                    <v-layout column align-center justify-center>
-                        <div class="headline white--text mb-3 text-xs-center">Web development has never been easier
-                        </div>
-                        <em>Kick-start your application today</em>
-                        <v-btn
-                                class="blue lighten-2 mt-5"
-                                dark
-                                large
-                                href="/pre-made-themes"
-                        >
-                            Get Started
-                        </v-btn>
-                    </v-layout>
-                </v-parallax>
-            </section>
-
-            <section>
-                <v-container grid-list-xl>
-                    <v-layout row wrap justify-center class="my-5">
-                        <v-flex xs12 sm4>
-                            <v-card class="elevation-0 transparent">
-                                <v-card-title primary-title class="layout justify-center">
-                                    <div class="black--text headline">Company info</div>
-                                </v-card-title>
-                                <v-card-text class="black--text">
-                                    Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
-                                    egestas.
-                                    Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse
-                                    potenti.
-                                </v-card-text>
-                            </v-card>
-                        </v-flex>
-                        <v-flex xs12 sm4>
-                            <v-card class="elevation-0 transparent">
-                                <v-card-title primary-title class="layout justify-center">
-                                    <div class="black--text headline">Contact us</div>
-                                </v-card-title>
-                                <v-card-text class="black--text">
-                                    Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                                </v-card-text>
-                                <v-list class="black--text transparent">
-                                    <v-list-tile>
-                                        <v-list-tile-action>
-                                            <v-icon class="blue--text">phone</v-icon>
-                                        </v-list-tile-action>
-                                        <v-list-tile-content>
-                                            <v-list-tile-title>777-867-5309</v-list-tile-title>
-                                        </v-list-tile-content>
-                                    </v-list-tile>
-                                    <v-list-tile>
-                                        <v-list-tile-action>
-                                            <v-icon class="blue--text">place</v-icon>
-                                        </v-list-tile-action>
-                                        <v-list-tile-content>
-                                            <v-list-tile-title>Chicago, US</v-list-tile-title>
-                                        </v-list-tile-content>
-                                    </v-list-tile>
-                                    <v-list-tile>
-                                        <v-list-tile-action>
-                                            <v-icon class="blue--text">email</v-icon>
-                                        </v-list-tile-action>
-                                        <v-list-tile-content>
-                                            <v-list-tile-title>john@vuetifyjs.com</v-list-tile-title>
-                                        </v-list-tile-content>
-                                    </v-list-tile>
-                                </v-list>
-                            </v-card>
-                        </v-flex>
-                    </v-layout>
-                </v-container>
-            </section>
-        </v-content>
+            </v-content>
+        </v-container>
     </v-app>
 </template>
