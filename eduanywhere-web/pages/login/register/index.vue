caed59ab86512d6603bcc4ba09fd3c703e3712dc
Josua Botha <josua@witklippie.co.za>
2018-08-30 11:09:29 +0200

    Add columns for rigister page course list

--- eduanywhere-web/pages/login/register/index.vue
+++ eduanywhere-web/pages/login/register/index.vue
@@ -1,6 +1,6 @@
 <template>
 <v-container>
-<v-flex xs12 sm6 md4 lg4 offset-sm3 offset-md4 offset-lg4>
+<v-flex xs12 sm12 md10 lg8 offset-md1 offset-lg2>
    <v-stepper v-model="e1">
     <v-stepper-header>
       <v-stepper-step :complete="e1 > 1" step="1">Register</v-stepper-step>
@@ -17,53 +17,54 @@
     <v-stepper-items>
       <v-stepper-content step="1">
         <v-card
-style="padding: 20px; margin-bottom: 10px"
-color="grey lighten-3"
->
-  <v-card-title>
-      <h2>New Account</h2>
-  </v-card-title>
-  <v-form v-model="valid">
-    <v-text-field
-      v-model="name"
-      :rules="nameRules"
-      label="Name"
-      required
-    ></v-text-field>
-    <v-text-field
-      v-model="email"
-      :rules="emailRules"
-      label="E-mail"
-      required
-    ></v-text-field>
-    <v-text-field
-      v-model="number"
-      :rules="numberRules"
-      label="Contact Number"
-      required
-    ></v-text-field>
-    <v-text-field
-      v-model="location"
-      :rules="locationRules"
-      label="Country"
-      required
-    ></v-text-field>
-    <v-text-field
-      v-model="password"
-      :rules="passwordRules"
-      :type="'password'"
-      label="Password"
-      required
-    ></v-text-field>
-    <v-text-field
-      v-model="password"
-      :rules="confirmPasswordRules"
-      :type="'password'"
-      label="Confirm Password"
-      required
-    ></v-text-field>
-  </v-form>
-</v-card>
+          style="padding: 20px; margin-bottom: 10px"
+          color="grey lighten-3"
+          >
+          <v-card-title>
+              <h2>New Account</h2>
+          </v-card-title>
+
+          <v-form v-model="valid">
+            <v-text-field
+              v-model="name"
+              :rules="nameRules"
+              label="Name"
+              required
+            ></v-text-field>
+            <v-text-field
+              v-model="email"
+              :rules="emailRules"
+              label="E-mail"
+              required
+            ></v-text-field>
+            <v-text-field
+              v-model="number"
+              :rules="numberRules"
+              label="Contact Number"
+              required
+            ></v-text-field>
+            <v-text-field
+              v-model="location"
+              :rules="locationRules"
+              label="Country"
+              required
+            ></v-text-field>
+            <v-text-field
+              v-model="password"
+              :rules="passwordRules"
+              :type="'password'"
+              label="Password"
+              required
+            ></v-text-field>
+            <v-text-field
+              v-model="password"
+              :rules="confirmPasswordRules"
+              :type="'password'"
+              label="Confirm Password"
+              required
+            ></v-text-field>
+          </v-form>
+        </v-card>
 
         <v-btn
           color="primary"
@@ -73,77 +74,66 @@ color="grey lighten-3"
         </v-btn>
 
         <v-btn flat>Cancel</v-btn>
+
       </v-stepper-content>
 
       <v-stepper-content step="2">
-  
-        <v-list>
-            <v-list-tile @click="chemistryChecked = !chemistryChecked">
-                <v-list-tile-action>
-                    <v-icon color="blue darken-4">{{getCheckBox(chemistryChecked)}}</v-icon>
-                </v-list-tile-action>
-                <v-list-tile-content>
-                    <v-list-tile-title>
-                        Chemistry
-                    </v-list-tile-title>
-                </v-list-tile-content>
-            </v-list-tile>
+        <v-layout row wrap>
+          <v-flex lg4 md4 sm6 xs12>
+            <h3>IGCSE</h3>
+            <v-list>
+              <v-list-tile @click="chemistryChecked = !chemistryChecked">
 
-            <v-list-tile @click="physicsChecked = !physicsChecked">
-                <v-list-tile-action>
-                    <v-icon color="blue darken-3">{{getCheckBox(physicsChecked)}}</v-icon>
-                </v-list-tile-action>
-                <v-list-tile-content>
-                    <v-list-tile-title>
-                        Physics
-                    </v-list-tile-title>
-                </v-list-tile-content>
-            </v-list-tile>
-            
-            <v-list-tile @click="biologyChecked = ! biologyChecked">
-                <v-list-tile-action>
-                    <v-icon color="blue darken-1">{{getCheckBox(biologyChecked)}}</v-icon>
-                </v-list-tile-action>
-                <v-list-tile-content>
-                    <v-list-tile-title>
-                        Biology
-                    </v-list-tile-title>
-                </v-list-tile-content>
-            </v-list-tile>
-                        <v-list-tile @click="chemistryChecked = !chemistryChecked">
-                <v-list-tile-action>
-                    <v-icon color="blue darken-4">{{getCheckBox(chemistryChecked)}}</v-icon>
-                </v-list-tile-action>
-                <v-list-tile-content>
-                    <v-list-tile-title>
-                        Afrikaans
-                    </v-list-tile-title>
-                </v-list-tile-content>
-            </v-list-tile>
+                  <v-list-tile-action>
+                      <v-icon color="blue darken-4">{{getCheckBox(chemistryChecked)}}</v-icon>
+                  </v-list-tile-action>
 
-            <v-list-tile @click="physicsChecked = !physicsChecked">
-                <v-list-tile-action>
-                    <v-icon color="blue darken-3">{{getCheckBox(physicsChecked)}}</v-icon>
-                </v-list-tile-action>
-                <v-list-tile-content>
-                    <v-list-tile-title>
-                        English
-                    </v-list-tile-title>
-                </v-list-tile-content>
-            </v-list-tile>
-            
-            <v-list-tile @click="biologyChecked = ! biologyChecked">
-                <v-list-tile-action>
-                    <v-icon color="blue darken-1">{{getCheckBox(biologyChecked)}}</v-icon>
-                </v-list-tile-action>
-                <v-list-tile-content>
-                    <v-list-tile-title>
-                        Mathematics
-                    </v-list-tile-title>
-                </v-list-tile-content>
-            </v-list-tile>
-        </v-list>
+                  <v-list-tile-content>
+                      <v-list-tile-title>
+                          Chemistry
+                      </v-list-tile-title>
+                  </v-list-tile-content>
 
+              </v-list-tile>
+            </v-list>
+          </v-flex>
+          <v-flex lg4 md4 sm6 xs12>
+            <h3>AS-Level</h3>
+            <v-list>
+              <v-list-tile @click="chemistryChecked = !chemistryChecked">
+
+                  <v-list-tile-action>
+                      <v-icon color="blue darken-4">{{getCheckBox(chemistryChecked)}}</v-icon>
+                  </v-list-tile-action>
+ 
+                  <v-list-tile-content>
+                      <v-list-tile-title>
+                          Mathematics
+                      </v-list-tile-title>
+                  </v-list-tile-content>
+                  
+              </v-list-tile>
+            </v-list>
+          </v-flex>
+          <v-flex lg4 md4 sm6 xs12>
+            <h3>A-Level</h3>
+            <v-list>
+              <v-list-tile @click="chemistryChecked = !chemistryChecked">
+
+                  <v-list-tile-action>
+                      <v-icon color="blue darken-4">{{getCheckBox(chemistryChecked)}}</v-icon>
+                  </v-list-tile-action>
+
+                  <v-list-tile-content>
+                      <v-list-tile-title>
+                          Afrikaans
+                      </v-list-tile-title>
+                  </v-list-tile-content>
+                  
+              </v-list-tile>
+            </v-list>
+          </v-flex>
+        </v-layout>
         <v-btn
           color="primary"
           @click="e1 = 3"
